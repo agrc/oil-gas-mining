d87b2f707193cf9cc68f6da7e9a5f3465965a9be
Scott Davis <stdavis@utah.gov>
2016-09-01 15:39:26 -0600

    more queries for Diana M

--- scripts/QueryLayers.sql
+++ scripts/QueryLayers.sql
@@ -1,7 +1,8 @@
 # this is a query used for a query layer that Diana Mason uses to generate maps.
 # She got tired of waiting for SGID data to be updated so we pointed her directly at the OGM database.
-# lines
-select top 100 geometry::STLineFromText(CONCAT('LINESTRING (', COORDS_SURF_E, ' ', COORDS_SURF_N, ', ', COORDS_BHL_E, ' ', COORDS_BHL_N, ')'), 26912) as geometry, API
+
+# Query Layer
+select geometry::STLineFromText(CONCAT('LINESTRING (', COORDS_SURF_E, ' ', COORDS_SURF_N, ', ', COORDS_BHL_E, ' ', COORDS_BHL_N, ')'), 26912) as geometry, API, GIS_STAT_TYPE
 from vw_AGRC_Well_Data
 where
 COORDS_BHL_N IS NOT NULL and
@@ -10,3 +11,7 @@ COORDS_SURF_N IS NOT NULL and
 COORDS_SURF_E IS NOT NULL and
 COORDS_BHL_N != COORDS_SURF_N and
 COORDS_BHL_E != COORDS_SURF_E
+
+
+# Query Layer Selection
+select geometry::STPointFromText(CONCAT('POINT(', COORDS_SURF_E, ' ', COORDS_SURF_N, ')'), 26912) as geometry, API, WELL_NAME, TOWNSHIP, RANGE, SECTION, COMPANY_NAME from vw_AGRC_Well_Data
